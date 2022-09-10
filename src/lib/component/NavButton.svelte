93120add23366147befab321bbdab76f2c7de43d
Alfiizzi21 <alfi.izzi.jalhik@gmail.com>
2022-09-10 09:48:18 +0700

    Memperbaiki hover nav button

--- src/lib/component/NavButton.svelte
+++ src/lib/component/NavButton.svelte
@@ -9,7 +9,7 @@
 	href="{host}/dashboard/{link}"
 	class="group flex flex-col justify-center items-center cursor-pointer bg-sky-700 w-full py-1"
 >
-	<span class="material-icons text-4xl group-hover:text-sky-300 transition-colors delay-300">
+	<span class="material-icons text-4xl group-hover:text-sky-300 transition-colors duration-300">
 		{icon}
 	</span>
 	<div class="text-xs capitalize">
